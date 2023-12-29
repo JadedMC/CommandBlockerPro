227a3ff104cfc037941252bb34ca332f21412df9
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2023-12-28 19:27:31 -0500

    Initial commit

---
+++ src/main/java/net/jadedmc/commandblockerpro/CommandBlockerProPlugin.java
@@ -0,0 +1,12 @@
+package net.jadedmc.commandblockerpro;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CommandBlockerProPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
